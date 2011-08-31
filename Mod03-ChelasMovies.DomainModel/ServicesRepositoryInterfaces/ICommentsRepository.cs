ba3f40ab4eb326055f6fb81f439a0b9a3560a06a
Luis Pereira <luis.mdc.pereira@hotmail.com>
2011-08-31 18:54:08 +0100

    Delete All

--- Mod03-ChelasMovies.DomainModel/ServicesRepositoryInterfaces/ICommentsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mod03_ChelasMovies.Rep;
-
-namespace Mod03_ChelasMovies.DomainModel.ServicesRepositoryInterfaces
-{
-    public interface ICommentsRepository : IRepository<Comment, int>
-    {
-
-    }
-}
--- Mod03-ChelasMovies.WebApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mod03_ChelasMovies.WebApp.MvcApplication" Language="C#" %>
